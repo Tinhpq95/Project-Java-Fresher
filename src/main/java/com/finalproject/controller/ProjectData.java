f5eecd21dbf3c3aa43394e699b6956274de7165d
training <training>
2017-08-28 14:09:45 +0700

    Cron job get Database

--- src/main/java/com/finalproject/controller/ProjectData.java
+++ src/main/java/com/finalproject/controller/ProjectData.java
@@ -8,12 +8,18 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.finalproject.model.ProjectInfo;
+import com.finalproject.responsitory.ProjectCompany;
 
+@Component
 public class ProjectData {
-	static int numberProjectTestSuite =0; // Number of project test suit from begin count to now
-	
+	private int numberProjectTestSuite = 0; // Number of project test suit from
+											// begin count to now
+
 	private List<String> getProjectName() {
 
 		List<String> projectName = new ArrayList<>();
@@ -38,12 +44,12 @@ public class ProjectData {
 	}
 
 	public List<ProjectInfo> getProjectInfo() {
-		
+
 		List<ProjectInfo> listProjectInfo = new ArrayList<>();
 		List<String> projectName = getProjectName();
 		List<String> projectTestSuitName = new ArrayList<>();
 		String url = "http://localhost/FrontPage.";
-		
+
 		for (int i = 0; i < projectName.size(); i++) {
 			int countDesigned = 0, countReady = 0, countManual = 0, countAutomated = 0;
 			try {
@@ -54,62 +60,70 @@ public class ProjectData {
 				for (int j = 1; j < tableRowElements.size(); j++) {
 					projectTestSuitName.add(url + projectName.get(i) + "."
 							+ tableRowElements.get(j).select("td").get(0).text().substring(1));
-					// System.out.println(tableRowElements.get(j).select("td").get(0).text().substring(1));
+					
 				}
 
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-	//		 System.out.println( projectName.get(i));
-			 
-			for (int k=numberProjectTestSuite ; k< projectTestSuitName.size(); k++){
-//			 System.out.println( projectTestSuitName.get(k));
-			 try {
-//					System.out.println(projectTestSuitName.get(k));
-						Document doc = Jsoup.connect(projectTestSuitName.get(k)).get();
-						Elements tableElements = doc.select("table");
-						Elements tableRowElements = tableElements.select(":not(thead) tr");
 
-						for (int n = 2; n < tableRowElements.size(); n++) {
+			for (int k = numberProjectTestSuite; k < projectTestSuitName.size(); k++) {
 
-							Element row = tableRowElements.get(n);
-							Elements rowItems = row.select("td");
-							for (int l = 0; l < rowItems.size(); l++) {
-								if ("<td>Automated?</td>".equals(tableRowElements.get(0).select("td").get(l).toString())) {
-									if ("Yes".equals(rowItems.get(l).text())) {
-										countAutomated++;
-									}
-									else{
-										countReady++;
-									}
-								}
-								if ("<td>Test Case Readiness</td>"
-										.equals(tableRowElements.get(0).select("td").get(l).toString())) {
-									if ("Designed".equals(rowItems.get(l).text())) {
-										countDesigned++;
-									}else{
-										countManual++;
-									}
+				try {
+					// System.out.println(projectTestSuitName.get(k));
+					Document doc = Jsoup.connect(projectTestSuitName.get(k)).get();
+					Elements tableElements = doc.select("table");
+					Elements tableRowElements = tableElements.select(":not(thead) tr");
+
+					for (int n = 2; n < tableRowElements.size(); n++) {
+
+						Element row = tableRowElements.get(n);
+						Elements rowItems = row.select("td");
+						for (int l = 0; l < rowItems.size(); l++) {
+							if ("<td>Automated?</td>".equals(tableRowElements.get(0).select("td").get(l).toString())) {
+								if ("Yes".equals(rowItems.get(l).text())) {
+									countAutomated++;
+								} else {
+									countManual++;
 								}
 							}
+							if ("<td>Test Case Readiness</td>"
+									.equals(tableRowElements.get(0).select("td").get(l).toString())) {
+								if ("Designed".equals(rowItems.get(l).text())) {
+									countDesigned++;
+								} else {
 
+									countReady++;
+								}
+							}
 						}
-						
 
-					} catch (IOException e) {
-						e.printStackTrace();
 					}
 
-			 }
-//			System.out.println("---->"+projectName.get(i));
-//			System.out.println("Designed: " + countDesigned);
-//			System.out.println("Manual: " + countManual);
-//			System.out.println("Automated: " + countAutomated);
-//			System.out.println("Ready: " + countReady);
-			listProjectInfo.add(new ProjectInfo(projectName.get(i), countDesigned, countReady, countManual, countAutomated));
-			numberProjectTestSuite = numberProjectTestSuite+projectTestSuitName.size();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+			listProjectInfo
+					.add(new ProjectInfo(projectName.get(i), countDesigned, countReady, countManual, countAutomated));
+			numberProjectTestSuite = numberProjectTestSuite + projectTestSuitName.size();
 		}
-		
+
 		return listProjectInfo;
+	}
+	
+	@Autowired
+	ProjectCompany projectCompany;
+	
+	@Scheduled(fixedRate = 600000)
+	public void reportCurrentTime() {
+		ProjectData listData = new ProjectData();
+		List<ProjectInfo> listProjectInfo = listData.getProjectInfo();
+		for (int i = 0; i < listProjectInfo.size(); i++) {
+			projectCompany.save(listProjectInfo.get(i));
+			listProjectInfo.get(i).toStringKQ();
+			System.out.println("AUTOOOOO");
+		}
 	}
 }
--- src/main/java/com/finalproject/controller/ProjectInfoController.java
+++ src/main/java/com/finalproject/controller/ProjectInfoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,26 +14,16 @@ import com.finalproject.responsitory.ProjectCompany;
 
 @RestController
 public class ProjectInfoController {
-	
+
 	@Autowired
 	ProjectCompany projectCompany;
-	
+
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/project/getAll")
-	public ResponseEntity<?> getAll(){
-		
+	public ResponseEntity<?> getAll() {
+
 		List<ProjectInfo> projectInfos = (List<ProjectInfo>) projectCompany.findAll();
 		return ResponseEntity.ok(projectInfos);
 	}
-	
-	@RequestMapping(value = "/project/create4test")
-	public void create4Test(){
-		ProjectData listData = new ProjectData();
-		List<ProjectInfo> listProjectInfo = listData.getProjectInfo();
-		for (int i =0; i< listProjectInfo.size(); i++){
-			projectCompany.save(listProjectInfo.get(i));
-			listProjectInfo.get(i).toStringKQ();
-		}
-	}
-	
-	
+
 }
