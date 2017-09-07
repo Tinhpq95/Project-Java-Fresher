ce6556243e306f7357738d8972731b24df6fdfb8
Hoang Ha <hoanghahuy2602@gmail.com>
2017-09-07 15:34:26 +0700

    Chinh sua code html

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -9,72 +9,85 @@ app.service('sharedProperties', function() {
 		},
 		setProjectName : function(name) {
 			projectname = name;
-			
+
 		}
 	};
 });
 
-app.controller('MainCtrl', function($scope, $http, sharedProperties) {
-	$scope.records = {};
-	$http.get('http://localhost:7000/project/getAllProjectName').then(
-			function(data) {
-				$scope.records = data.data;
-			})
+app.controller('MainCtrl',
+		function($rootScope, $scope, $http, sharedProperties) {
+			$scope.records = {};
+			$scope.nameProject = 'SuitProject001';
+			$http.get('http://localhost:7000/project/getAllProjectName').then(
+					function(data) {
+						$scope.records = data.data;
+					})
 
-	$scope.setCurrentProject = function(name) {
+			$scope.setCurrentProject = function(name) {
 
-		sharedProperties.setProjectName(name);
-		console.log(name);
+				sharedProperties.setProjectName(name);
+				$scope.nameProject = sharedProperties.getProjectName();
+				$rootScope.Get();
+				$rootScope.Get2();
 
-	}
-});
+			}
+		});
 
-app.controller('PieChart', function($scope, $http, sharedProperties) {
+app.controller('PieChart',
+		function($rootScope, $scope, $http, sharedProperties) {
 
-	google.charts.load('current', {
-		'packages' : [ 'corechart' ]
-	});
+			google.charts.load('current', {
+				'packages' : [ 'corechart' ]
+			});
 
-	// Function draw pie chart
-	google.charts.setOnLoadCallback(drawPieChart);
+			// Function draw pie chart
+			google.charts.setOnLoadCallback(drawPieChart);
 
-	function drawPieChart() {
-		var data = new google.visualization.DataTable();
-		data.addColumn('string', 'Status');
-		data.addColumn('number', 'Quantity');
-		data.addRows($scope.Excution);
+			function drawPieChart() {
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Status');
+				data.addColumn('number', 'Quantity');
+				data.addRows($scope.Excution);
 
-		var options = {
-			title : 'Test Execution',
-			is3D : true,
-		};
+				var options = {
+					title : 'Automation',
+					is3D : true,
+					width : 550,
+					height : 425,
+					chartArea : {
+						left : 10,
+						top : 120,
+					},
+				};
 
-		var chart = new google.visualization.PieChart(document
-				.getElementById('piechart_3d'));
-		chart.draw(data, options);
-	}
+				var chart = new google.visualization.PieChart(document
+						.getElementById('piechart_3d'));
+				chart.draw(data, options);
+			}
 
-	// Get content from JSON url
-	$scope.Excution = [];
-	$scope.Get = function() {
-		$http(
-				{
-					method : "GET",
-					url : "http://localhost:7000/project/"
-							+ sharedProperties.getProjectName()
-				}).then(
-				function mySuccess(response) {
+			// Get content from JSON url
+			$scope.Excution = [];
+			$rootScope.Get = function() {
+				$http(
+						{
+							method : "GET",
+							url : "http://localhost:7000/project/"
+									+ sharedProperties.getProjectName()
+						}).then(
+						function mySuccess(response) {
 
-					$scope.Excution = [ [ "designed", response.data.designed ],
-							[ "ready", response.data.ready ] ];
-					drawPieChart();
-				});
-	}
+							$scope.Excution = [
+									[ "Manual", response.data.manual ],
+									[ "Automated", response.data.automated ] ];
+							drawPieChart();
+						});
+			}
+			$rootScope.Get();
 
-	$scope.Get();
-});
+		});
 
-app.controller('columnchart', function($scope, $http, sharedProperties) {
+app.controller('columnchart', function($rootScope, $scope, $http,
+		sharedProperties) {
 
 	google.charts.load('current', {
 		'packages' : [ 'corechart' ]
@@ -89,14 +102,19 @@ app.controller('columnchart', function($scope, $http, sharedProperties) {
 		data.addRows($scope.Excution);
 
 		var options = {
-			title : "Density of Precious Metals, in g/cm^3",
-			width : 600,
-			height : 400,
-			bar : {
-				groupWidth : "95%"
+			title : "Design Status",
+			backgroundColor :{
+				fill:'#CFD8DC',
+				opacity:100,
 			},
+			width : 400,
+			height : 400,
 			legend : {
 				position : "none"
+			},
+			chartArea : {
+				left : 80,
+				top : 100,
 			},
 		};
 
@@ -107,7 +125,7 @@ app.controller('columnchart', function($scope, $http, sharedProperties) {
 
 	// Get content from JSON url
 	$scope.Excution = [];
-	$scope.Get = function() {
+	$rootScope.Get2 = function() {
 		$http(
 				{
 					method : "GET",
@@ -116,11 +134,11 @@ app.controller('columnchart', function($scope, $http, sharedProperties) {
 				}).then(
 				function mySuccess(response) {
 
-					$scope.Excution = [ [ "auto", response.data.automated ],
-							[ "manual", response.data.manual ] ];
+					$scope.Excution = [ [ "Designed", response.data.designed ],
+							[ "Ready", response.data.ready ] ];
 					drawColumnchart();
 				});
 	}
+	$rootScope.Get2();
 
-	$scope.Get();
 });
